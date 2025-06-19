cfb0ae5ab7a1b5a629f3a56a93db98ab9602bc35
Lai Hong Yu <joyjay606@gmail.com>
2025-06-19 11:05:44 +0800

    Initial Commit

---
+++ oral/oralApp.swift
@@ -0,0 +1,17 @@
+//
+//  oralApp.swift
+//  oral
+//
+//  Created by Lai Hong Yu on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct oralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
