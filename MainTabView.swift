3015e2dfd810a9a25d1b4a8bc6fceb86034d6141
Abinav Gopi <abinav.gopi@techtime.coffee>
2025-07-07 21:46:03 +0800

    Guys I literally finished up MainTabView and uhh the renaming of files and the renaming 2 views and dependednt views I will finish up by tmr night. And the ones reminded to me today by tmr meeting

---
+++ MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  oral
+//
+//  Created by Abinav Gopi on 7/7/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
